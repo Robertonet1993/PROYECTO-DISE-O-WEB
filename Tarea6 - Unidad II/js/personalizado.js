c476a4b5543e94d50bab145d38d860829ee38335
Robertonet1993 <134185156+Robertonet1993@users.noreply.github.com>
2023-05-25 02:25:30 -0600

    Delete Tarea6 - Unidad II directory

--- Tarea6 - Unidad II/js/personalizado.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function () {            
-    if($(document).scrollTop()>10){                
-        $('#menuStiky').addClass("banner-background");
-    }else{
-        $('#menuStiky').removeClass("banner-background");
-    }
-})
